0d5e13d9f7bd6e32c9b694f9394fd30684dc1d8a
weeclicks <weeclicks@Kais-Mac-mini.local>
2018-02-14 10:17:42 +0800

    win transition 2

--- js/coil.js
+++ js/coil.js
@@ -389,12 +389,11 @@ var Coil = (function(){
 		
 		playing = false;
 		menu.fadeIn( MENU_FADE_IN_DURATION );
-		window.webkit.messageHandlers.score.postMessage(Math.floor(score));
-		// Add transition here ?
-		// call api score
-		if (score > 1) {
+		if (Math.floor(score) > 1) {
 			window.open("win.html", "_self");
 		} 
+		window.webkit.messageHandlers.score.postMessage(Math.floor(score));
+
 		
 	}
 	
